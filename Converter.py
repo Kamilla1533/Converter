883b3f3c273be8b1caf958a7795658bcb981f8a4
Kamilla1533 <155493339+Kamilla1533@users.noreply.github.com>
2024-01-09 15:35:46 +0300

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -6,15 +6,25 @@ class TemperatureConverterGUI:
         self.main_window = tkinter.Tk()
         self.main_window.title("Converter")
 
-        # Верхние виджеты
-        self.top_frame = tkinter.Frame(self.main_window)
-        self.bottom_frame = tkinter.Frame(self.main_window)
+        self.top_frame = tkinter.Frame()
+        self.mid_frame = tkinter.Frame()
+        self.bottom_frame = tkinter.Frame()
 
-        self.prompt_label = tkinter.Label(self.top_frame, text = "Введите температуру в Цельсиях:")
+        # Верхние виджеты
+        self.prompt_label = tkinter.Label(self.top_frame, text = "Введите температуру в градусах по Цельсию:")
         self.temp_entry = tkinter.Entry(self.top_frame, width = 10)
 
         self.prompt_label.pack(side="left")
         self.temp_entry.pack(side="left")
+
+        # Серединные виджеты
+        self.descr_label = tkinter.Label(self.mid_frame, text="Преобразовано в градусы по Фаренгейту:")
+
+        self.value = tkinter.StringVar()
+        self.celsius_label = tkinter.Label(self.mid_frame, textvariable=self.value)
+
+        self.descr_label.pack(side = "left")
+        self.celsius_label.pack(side = "left")
 
         # Нижние виджеты
         self.calc_button = tkinter.Button(self.main_window, text="Преобразовать", command=self.convert)
@@ -24,6 +34,7 @@ class TemperatureConverterGUI:
         self.calc_button.pack(side="bottom")
 
         self.top_frame.pack()
+        self.mid_frame.pack()
         self.bottom_frame.pack()
 
         tkinter.mainloop()
@@ -31,7 +42,8 @@ class TemperatureConverterGUI:
     def convert(self):
         celsius = float(self.temp_entry.get())
         fr = (celsius * 9 / 5) + 32
-        tkinter.messagebox.showinfo("Результаты", str(celsius) + " градусов по Цельсию эквивалетно " + str(fr) + " градусам по Фаренгейту")
+
+        self.value.set(str(fr))
 
 if __name__ == '__main__':
     temp_conv = TemperatureConverterGUI()
